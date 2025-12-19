82d06b40b39004d076a7d2778f3ebe9de77a1361
gandhirajj <gandhiraj.j2023aiml@sece.ac.in>
2025-12-19 15:07:15 +0530

    add axios client

---
+++ src/utils/api.js
@@ -0,0 +1,13 @@
+const axios = require('axios').default;
+
+const baseURL = process.env.REACT_APP_API_URL || 'https://medical-bookings-2.onrender.com';
+
+const api = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+module.exports = api;
