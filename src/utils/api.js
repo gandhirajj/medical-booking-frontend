41ffda2285b551bbf3854b8c173b4a400a15e4f3
gandhirajj <153410926+gandhirajj@users.noreply.github.com>
2025-12-19 15:12:22 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,11 @@
-const axios = require('axios').default;
+import axios from 'axios';
 
 const baseURL = process.env.REACT_APP_API_URL || 'https://medical-bookings-2.onrender.com';
 
 const api = axios.create({
   baseURL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
 
-module.exports = api;
+export default api;
